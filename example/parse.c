7e13bbab614ec8909ab40682ff57e09f49d0900b
hdthky <hdthky@outlook.com>
2021-06-18 06:34:35 -0700

    modify example/parse.c for fuzzing purpose

--- example/parse.c
+++ example/parse.c
@@ -10,15 +10,15 @@ int main(int argc, char * argv[])
     dictionary * ini ;
     char       * ini_name ;
 
-    if (argc<2) {
-        ini_name = "twisted.ini";
-    } else {
+    if (argc == 2) {
         ini_name = argv[1] ;
-    }
-
-    ini = iniparser_load(ini_name);
-    iniparser_dump(ini, stdout);
-    iniparser_freedict(ini);
+        ini = iniparser_load(ini_name);
+        if (ini==NULL) {
+            return -1 ;
+        }
+        iniparser_dump(ini, stdout);
+        iniparser_freedict(ini);
+    } 
 
     return 0 ;
 }
